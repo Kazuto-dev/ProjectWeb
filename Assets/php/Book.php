05a71ec8fd46ac8982de6bdf721739df7ef41290
Jandel Isidro <kazutokirigayakirito60@gmail.com>
2023-06-17 22:17:02 +0800

    6th commit

--- Assets/php/Book.php
+++ Assets/php/Book.php
@@ -10,7 +10,7 @@ $un = reflectUser();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Proguides</title>
-    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../css/features.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome/5.15.3/css/all.min.css">
