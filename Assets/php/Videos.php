88a2eb484715e10f44ed0cd774d922d0e877c72b
MarkAllenG <94668277+Gallenark@users.noreply.github.com>
2023-06-18 10:35:12 +0800

    HAHAHAHAHA

--- Assets/php/Videos.php
+++ Assets/php/Videos.php
@@ -11,7 +11,7 @@ sessionTimeout();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Proguides</title>
-    <link rel="stylesheet" href="../css/Home.css">
+    <link rel="stylesheet" href="../css/video.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome/5.15.3/css/all.min.css">
